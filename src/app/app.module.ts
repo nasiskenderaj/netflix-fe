2baaf600a0a6e131866cda780a36b32485b22f92
Nasibi Skenderaj <nskenderaj@qualysoft-coe.com>
2022-07-04 17:07:53 +0200

    adding navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {AuthModule} from "./auth/auth/auth.module";
+import {RouterModule} from "@angular/router";
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import {AppRoutingModule} from "./app-routing.module";
+
 
 @NgModule({
   declarations: [
@@ -10,6 +14,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    AuthModule,
+    RouterModule,
+    FontAwesomeModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/auth/auth/auth-routing.module.ts
+++ src/app/auth/auth/auth-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AuthComponent} from "./auth/auth.component";
 
 const routes: Routes = [
   {
-    path: '',
-    loadChildren:()=> import('./auth/auth/auth.module').then(m=>m.AuthModule)
+    path:'',component:AuthComponent
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AuthRoutingModule { }
