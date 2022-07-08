267cc76fabfef690ee769dc81d8fe16e761a6fb4
Nasibi Skenderaj <nskenderaj@qualysoft-coe.com>
2022-07-08 09:48:15 +0200

    prepare profile component

---
+++ src/app/shared/ui-grid/ui-grid.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ui-grid',
+  templateUrl: './ui-grid.component.html',
+  styleUrls: ['./ui-grid.component.css']
+})
+export class UiGridComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
